8a2400629a22a06d2ec93065d9f775d29b0739f9
rpedraza01 <rpedraza01@gmail.com>
2019-08-13 11:16:04 -0700

    Airship web notification test

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1565718477.76
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://rodrigopedraza.com',
+  defaultTitle: 'Test Notification',
+  defaultActionURL: 'https://rodrigopedraza.com',
+  appKey: 'WMjUdzFkTPqO0xKQeaEEkA',
+  token: 'MTpXTWpVZHpGa1RQcU8weEtRZWFFRWtBOmFIclhwZDhCV0RQdDlNVDRyc1pXcDV2VENONjlsbFBiaXBWQWJXM2hzNWs',
+  vapidPublicKey: 'BF3FdEoACzsYh70KwAejWQj8uYWrCM2_93nEHxNdl3Vv5uidDGDm9rvztowgFXqKVulgjBXcn0ckcqyj-dkwM-Y='
+})
